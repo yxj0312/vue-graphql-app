e5736bf1ec9d9a1126cb00c85aead608d5060427
yxj0312 <yanxiajun@hotmail.com>
2022-02-21 23:03:12 +0100

    set appolloclient

--- src/main.js
+++ src/main.js
@@ -1,4 +1,40 @@
 import { createApp } from 'vue'
+import { 
+    ApolloClient, 
+    createHttpLink, 
+    InMemoryCache } 
+from '@apollo/client'
+
+import gql from 'graphql-tag'
+
 import App from './App.vue'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql'
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const ALL_BOOKS_QUERY = gql`
+    query Allbooks{
+        allBooks{
+            id
+            title
+            rating
+        }
+    }
+`
+apolloClient
+    .query({
+        query: ALL_BOOKS_QUERY,
+    })
+    .then(res => {
+        console.log(res)
+    })
 
 createApp(App).mount('#app')
