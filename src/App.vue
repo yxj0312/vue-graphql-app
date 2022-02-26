5732dede970a0779981842699ed4d15504b93958
yxj0312 <yanxiajun@hotmail.com>
2022-02-26 22:54:02 +0100

    add a error message

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <input type="search" v-model="searchTerm" />
     <p v-if="loading">Loading...</p>
+    <p v-else-if="error">Something went wrong! Please try again</p>
     <template v-else>
       <p v-for="book in result.allBooks" :key="book.id">
         {{ book.title }}
@@ -19,12 +20,12 @@ export default {
   name: "App",
   setup() {
     const searchTerm = ref("");
-    const { result, loading } = useQuery(ALL_BOOKS_QUERY, () => ({
+    const { result, loading, error } = useQuery(ALL_BOOKS_QUERY, () => ({
       search: searchTerm.value,
     }));
 
     console.log(result);
-    return { result, searchTerm, loading };
+    return { result, searchTerm, loading, error };
   },
 };
 </script>
